b113d952913b6af49507385b146c0ae76e49a2fb
Kira <thiennguyen.dev.0103@gmail.com>
2024-02-07 11:01:10 +0700

    feat: setup liveblocks

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -6,4 +6,10 @@ const Participants = () => {
   );
 };
 
+Participants.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="absolute right-2 top-2 flex h-12 w-[100px] items-center rounded-md bg-white px-1.5 shadow-md" />
+  );
+};
+
 export default Participants;
